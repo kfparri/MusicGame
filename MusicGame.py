7f8a2fa9c7a552913b40a694512739ce42d3f52e
Kyle Parrish <kfparri@gmail.com>
2015-05-25 10:09:31 -0500

    added the pre_init function for the mixer to make the sounds less laggy

--- MusicGame.py
+++ MusicGame.py
@@ -79,7 +79,9 @@ def drawMyRect(surface):
     return surface
 
 def main():
+    pygame.mixer.pre_init(44100,-16,2, 1024)
     pygame.init()
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption('Music Game')
 
